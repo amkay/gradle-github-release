adcc87bb23b32a08aa93a3fb4ee52990ae0fb208
Max Käufer <am-kay@web.de>
2015-08-07 19:57:04 +0200

    Add groovydocJar to default tasks to upload artifacts from

--- src/main/groovy/com/github/amkay/gradle/github/release/dsl/GithubReleaseExtension.groovy
+++ src/main/groovy/com/github/amkay/gradle/github/release/dsl/GithubReleaseExtension.groovy
@@ -15,7 +15,8 @@ class GithubReleaseExtension {
     static final         String             NAME                         = 'githubRelease'
     private static final Collection<String> DEFAULT_TASKS_TO_UPLOAD_FROM = [ 'jar',
                                                                              'sourcesJar',
-                                                                             'javadocJar' ]
+                                                                             'javadocJar',
+                                                                             'groovydocJar' ]
 
 
     protected Project project
