757af04df91280a0ee6b49f65498057b05700ebb
Max Käufer <am-kay@web.de>
2015-08-07 18:58:05 +0200

    Introduce upload in extension

--- src/main/groovy/com/github/amkay/gradle/github/release/dsl/GithubReleaseExtension.groovy
+++ src/main/groovy/com/github/amkay/gradle/github/release/dsl/GithubReleaseExtension.groovy
@@ -1,6 +1,9 @@
 package com.github.amkay.gradle.github.release.dsl
 
 import org.gradle.api.Project
+import org.gradle.api.file.CopySpec
+import org.gradle.util.ConfigureUtil
+
 /**
  * TODO
  *
@@ -15,11 +18,13 @@ class GithubReleaseExtension {
             String   user
             String   apiKey
             String   workingPath
+    final   CopySpec upload
 
 
     GithubReleaseExtension(final Project project) {
         this.project = project
         this.workingPath = "${project.buildDir.name}/github-release"
+        this.upload = project.copySpec { from project.tasks[ 'jar' ] }
     }
 
 
@@ -37,6 +42,10 @@ class GithubReleaseExtension {
 
     File getWorkingDir() {
         project.file workingPath
+    }
+
+    void upload(@DelegatesTo(CopySpec) final Closure cl) {
+        ConfigureUtil.configure cl, upload
     }
 
 }
