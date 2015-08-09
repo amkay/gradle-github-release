f7c324d46db40120517906bf5aeeb73e0728b61a
Max Käufer <am-kay@web.de>
2015-08-09 19:12:16 +0200

    Add default file to read description from & auto-discovery of it

--- src/main/groovy/com/github/amkay/gradle/github/release/dsl/GithubReleaseExtension.groovy
+++ src/main/groovy/com/github/amkay/gradle/github/release/dsl/GithubReleaseExtension.groovy
@@ -68,6 +68,12 @@ class GithubReleaseExtension {
         repositoryRoot = project.projectDir
 
         this.workingPath = "${project.buildDir.name}/$DEFAULT_WORKING_DIR"
+
+        def descriptionFile = project.file "${project.buildDir}/docs/CHANGES.md"
+        if (descriptionFile.exists()) {
+            description descriptionFile
+        }
+
         this.upload = project.copySpec {
             defaultTasksToUploadFrom.each { task ->
                 from task
